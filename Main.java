409b1ea2b5e8b332214c6402e01f14564f5ebd9b
khrid0328 <khrid0328@wrdsb.ca>
2020-10-07 01:49:49 +0000

    A5Q4 This program determines the final balance of an investment based on the money invested, interest rate and number of years.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This  program that asks the user how much money they wish to invest, what their interest rate is (as a full percentage), and how many years they want to invest. It will then determine what their final balance would be.
+ * @author Dafna Khripun
  */
 public class Main {
 
@@ -9,7 +10,38 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user what their starting balance is
+    System.out.println("Please enter a starting balance");
+
+    // declare a variable for users input
+    double balance = input.nextDouble();
+
+    // ask the user what their inteterest rate is
+    System.out.println("Please enter your interest rate");
+  
+    // declare a variable for users input
+    double interestRate = input.nextDouble();
+
+    // ask the user what numbers of year would they invest for
+    System.out.println("Please enter the number of years you will invest for");
+
+    // declare a variable for users input
+    int numYears = input.nextInt();
+
+    // use a loop to determine the final balance of the investment
+    for(int i = 1; i <= numYears; i++){
+
+      // calculate the amount of money in interest
+      double interest = balance * (interestRate / 100);
+
+      // calculate the final balance for this year
+      balance = balance + interest ;
+    } 
+    // print the final balance to the screen
+    System.out.println("Your final balance would be $" + balance);
   }
 }
